7ed2ac2a9e2a3ff2c5329239bfcc1e368694ec67
ewfx <chandra.punam8@gmail.com>
2025-03-25 19:15:20 +0530

    initial project

---
+++ code/src/LoanCreditRiskAssessment/LoanCreditRiskAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoanCreditRiskAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ code/src/LoanCreditRiskAssessment/LoanCreditRiskAssessment/Models/Loan.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.Models
+{
+    public class Loan
+    {
+        public int LoanId { get; set; }
+        public int CustomerId { get; set; }
+        public double LoanAmount { get; set; }
+        public int CreditScore { get; set; }
+        public string? EmploymentStatus { get; set; }
+        public double DebtToIncomeRatio { get; set; }
+        public string? ApprovalStatus { get; set; }
+        public string? LoanResult { get; set; }
+        public double InterestRate { get; set; }
+        public string? ExpectedResult { get; set; }
+    }
+}
