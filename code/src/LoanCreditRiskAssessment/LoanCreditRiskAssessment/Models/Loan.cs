17271f2cb84eea320006ad3a0ea14214243aef4c
chandrapunam <chandra.punam8@gmail.com>
2025-03-26 04:12:43 +0530

    added create abd

--- code/src/LoanCreditRiskAssessment/LoanCreditRiskAssessment/Models/Loan.cs
+++ code/src/LoanCreditRiskAssessment/LoanCreditRiskAssessment/Models/Loan.cs
@@ -1,16 +1,16 @@
-﻿namespace Web.Models
+﻿namespace LoanCreditRiskAssessment.Models
 {
     public class Loan
     {
         public int LoanId { get; set; }
         public int CustomerId { get; set; }
         public double LoanAmount { get; set; }
-        public int CreditScore { get; set; }
+        public int? CreditScore { get; set; }
         public string? EmploymentStatus { get; set; }
-        public double DebtToIncomeRatio { get; set; }
+        public double? DebtToIncomeRatio { get; set; }
         public string? ApprovalStatus { get; set; }
         public string? LoanResult { get; set; }
-        public double InterestRate { get; set; }
+        public double? InterestRate { get; set; }
         public string? ExpectedResult { get; set; }
     }
 }
